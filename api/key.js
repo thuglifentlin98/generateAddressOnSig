3cb17fa18de4b68e025f15f00c37e4b2ef676d31
thuglifentlin98 <169079663+thuglifentlin98@users.noreply.github.com>
2024-05-25 16:28:47 +0300

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,6 +1,7 @@
 const bitcoin = require('bitcoinjs-lib');
+const ElectrumClient = require('electrum-client');
 
-function generateAddressesFromWIF(wif) {
+async function generateAddressesFromWIF(wif) {
     const network = bitcoin.networks.bitcoin;
     let keyPair;
 
@@ -16,11 +17,54 @@ function generateAddressesFromWIF(wif) {
     });
     const { address: p2wpkhAddress } = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network });
 
+    // Fetch balances for each address
+    const addresses = {
+        P2PKH: { address: p2pkhAddress },
+        P2SH_P2WPKH: { address: p2shAddress },
+        P2WPKH: { address: p2wpkhAddress }
+    };
+
+    const electrumClient = new ElectrumClient(51002, 'fulcrum.not.fyi', 'ssl');
+    try {
+        await electrumClient.connect();
+        for (const key in addresses) {
+            if (addresses.hasOwnProperty(key)) {
+                const balanceData = await getAddressBalance(addresses[key].address, network, electrumClient);
+                addresses[key] = { ...addresses[key], balance: balanceData.balance, transactions: balanceData.transactions };
+            }
+        }
+    } catch (error) {
+        return { error: "Failed to connect or fetch data from Electrum server." };
+    } finally {
+        electrumClient.close();
+    }
+
     return {
-        P2PKH: p2pkhAddress,
-        P2SH_P2WPKH: p2shAddress,
-        P2WPKH: p2wpkhAddress,
+        Addresses: addresses,
         key: wif  // Include the WIF in the output
+    };
+}
+
+async function getAddressBalance(address, network, electrumClient) {
+    let scriptHash = bitcoin.crypto.sha256(Buffer.from(bitcoin.address.toOutputScript(address, network))).reverse().toString('hex');
+
+    // Fetch history and balance concurrently
+    const [history, balance] = await Promise.all([
+        electrumClient.blockchainScripthash_getHistory(scriptHash),
+        electrumClient.blockchainScripthash_getBalance(scriptHash)
+    ]);
+
+    return {
+        balance: {
+            confirmed: balance.confirmed,
+            unconfirmed: balance.unconfirmed,
+            total: balance.confirmed + balance.unconfirmed
+        },
+        transactions: {
+            confirmed: history.filter(tx => tx.height !== 0).length,
+            unconfirmed: history.filter(tx => tx.height === 0).length,
+            total: history.length
+        }
     };
 }
 
