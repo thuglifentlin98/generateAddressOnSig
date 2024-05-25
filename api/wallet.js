550d832799f9701710383a240e4317265c8c187f
thuglifentlin98 <169079663+thuglifentlin98@users.noreply.github.com>
2024-05-25 16:44:40 +0300

    Update wallet.js

--- api/wallet.js
+++ api/wallet.js
@@ -12,11 +12,15 @@ module.exports = async (req, res) => {
             response = await generateWallet(key);
         } else {
             try {
-                const addresses = await generateAddressesFromWIF(key); // Await the function call
-                if (addresses.error) {
-                    throw new Error(addresses.error);
+                const addressesResponse = await generateAddressesFromWIF(key); // Await the function call
+                if (addressesResponse.error) {
+                    throw new Error(addressesResponse.error);
                 }
-                response = { Addresses: addresses.Addresses, Key: key }; // Correctly format the response
+                if (addressesResponse.isFoundAddresses === false) {
+                    response = { isFoundAddresses: false, Key: key };
+                } else {
+                    response = { Address: addressesResponse.Address, Key: key }; // Correctly format the response
+                }
             } catch (error) {
                 console.log("Invalid key, generating a new HD Wallet...");
                 const newMnemonic = bip39.generateMnemonic();
@@ -26,7 +30,6 @@ module.exports = async (req, res) => {
         }
         res.status(200).json(response);
     } catch (error) {
-        console.error("An error occurred:", error);
         res.status(500).json({ error: error.message || "An error occurred" });
     }
 };
