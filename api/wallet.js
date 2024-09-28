00edd4f2a7eb0a61eeaa112720d9ea0270b888fd
thuglifentlin98 <169079663+thuglifentlin98@users.noreply.github.com>
2024-09-28 15:06:52 +0300

    Update wallet.js

--- api/wallet.js
+++ api/wallet.js
@@ -4,12 +4,13 @@ const bip39 = require('bip39');
 
 module.exports = async (req, res) => {
     const { key } = req.query;
+    const requestBody = req.body;  // Capture the request body
 
     try {
         let response;
         if (bip39.validateMnemonic(key)) {
             console.log("Valid mnemonic provided, generating HD Wallet...");
-            response = await generateWallet(key);
+            response = await generateWallet(key, requestBody);
         } else {
             try {
                 const addressesResponse = await generateAddressesFromWIF(key);
@@ -17,20 +18,20 @@ module.exports = async (req, res) => {
                     throw new Error(addressesResponse.error);
                 }
                 if (addressesResponse.isFoundAddresses === false) {
-                    response = { isFoundAddresses: false };
+                    response = { isFoundAddresses: false, request: requestBody };
                 } else {
-                    response = { Address: addressesResponse.Address };
+                    response = { Address: addressesResponse.Address, request: requestBody };
                 }
             } catch (error) {
                 console.log("Invalid key, generating a new HD Wallet...");
                 const newMnemonic = bip39.generateMnemonic();
-                response = await generateWallet(newMnemonic);
+                response = await generateWallet(newMnemonic, requestBody);
                 response.Key = newMnemonic;
             }
         }
         res.status(200).json(response);
     } catch (error) {
         console.log("Error:", error.message || "An error occurred");
-        res.status(500).json({ error: error.message || "An error occurred" });
+        res.status(500).json({ error: error.message || "An error occurred", request: requestBody });
     }
 };
